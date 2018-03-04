2698c63f9de977ebdcf9708e6ab0391d85fdf17c
kveca1 <kveca1@vols.utk.edu>
2018-03-03 18:45:22 -0600

    fixed app.py

--- app.py
+++ app.py
@@ -7,24 +7,24 @@ def index():
     return render_template("index.html")
 
 @app.route('/<roomID>/poll')
-def index():
+def poll():
     return 'poll in room number %d' % roomID
 
 @app.route('/<roomID>/poll/<pollID>')
-def index():
+def pollID():
     return 'poll in room number %d and poll number %d' % (roomID, pollID)
 
 @app.route('/<roomID>/question')
-def index():
+def question():
     return 'question in room number %d' % roomID
 
 @app.route('/<roomID>/poll/<pollID>/answer')
-def index():
+def answer():
     return 'answer to poll number %d in room number %d' % (pollID, roomID)
 
 @app.route('/register')
-def index():
-    return 'question in room number %d' % roomID
+def register():
+    return 'register'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
