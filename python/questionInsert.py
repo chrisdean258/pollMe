116cf0a21f2197402ac4e5489def434fafee3933
colsarcol <git@cmawhinney.com>
2018-03-04 02:34:48 -0600

    Added code to input info into ans db

---
+++ python/questionInsert.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import pymysql as PyMySQL
+import dbconn
+import datetime
+
+def askQuestion(phoneNumber, roomCode, message):
+	db = dbconn.dbcon()
+
+	tempTime = datetime.datetime.now()
+	curDate = tempTime.strftime("%Y-%m-%d")
+	curTime = tempTime.strftime("%H:%M:%S")
+	
+	cursor = db.cursor()
+	phoneNumber = phoneNumber
+	roomCode = roomCode
+	message = message
+
+	sql = """INSERT INTO ans(ansDate, ansTime, roomId, phone, message, ansClassId) VALUES ('"""+ curDate +"""', '"""+ curTime +"""', '"""+ roomCode +"""','"""+ phoneNumber +"""','"""+ message +"""','N/A')"""
+	try:
+		cursor.execute(sql)
+		db.commit()
+		print("Insert was fine")
+	except:
+		db.rollback()
+		print("Insert fucked")
+
+	db.close()	
+	return None
+		
+def main():
+	#phoneNumber = "8970987890"
+	#roomCode = "testCode"
+	#message = "TEST MESSAGE"
+
+	#askQuestion(phoneNumber, roomCode, message)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
