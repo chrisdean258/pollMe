51d0c66c0c4acfb2ec8f12d9191bf91feadb5fbf
kveca1 <kveca1@vols.utk.edu>
2018-03-03 18:42:25 -0600

    added crap to app.py

--- app.py
+++ app.py
@@ -6,6 +6,25 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/<roomID>/poll')
+def index():
+    return 'poll in room number %d' % roomID
+
+@app.route('/<roomID>/poll/<pollID>')
+def index():
+    return 'poll in room number %d and poll number %d' % (roomID, pollID)
+
+@app.route('/<roomID>/question')
+def index():
+    return 'question in room number %d' % roomID
+
+@app.route('/<roomID>/poll/<pollID>/answer')
+def index():
+    return 'answer to poll number %d in room number %d' % (pollID, roomID)
+
+@app.route('/register')
+def index():
+    return 'question in room number %d' % roomID
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
