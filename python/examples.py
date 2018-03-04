5578e7170cdea87e92c9339345cee4b3e531768b
chrisdean258 <chrisdean258@gmail.com>
2018-03-04 00:31:02 -0500

    im a better coder than you colin

--- python/examples.py
+++ python/examples.py
@@ -10,22 +10,18 @@ cursor = db.cursor()
 
 # Prepare SQL query to INSERT a record into the database.
 sql = "SELECT * FROM rooms"
-try:
-   # Execute the SQL command
-   cursor.execute(sql)
-   # Fetch all the rows in a list of lists.
-   results = cursor.fetchall()
-   print(results)
-   for row in results:
-      fname = row[0]
-      print(fname)
-   #   lname = row[1]
-   #   age = row[2]
-      # Now print fetched result
-   #   print ("fname = %s,lname = %s,age = %d" % \
-   #          (fname, lname, age ))
-except:
-   print ("Error: unable to fetch data")
+# Execute the SQL command
+cursor.execute(sql)
+# Fetch all the rows in a list of lists.
+results = cursor.fetchall()
+#print(results)
+for row in results:
+   fname = row[0]
+   lname = row[1]
+   age = row[2]
+   # Now print fetched result
+   print ("fname = %s,lname = %s,age = %s" % \
+          (fname, lname, age ))
 
 # disconnect from server
 db.close()
